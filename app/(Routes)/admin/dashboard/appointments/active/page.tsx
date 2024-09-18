904634d805981d136edfc40194fc91160562c3ee
Muse-feta <fetamuse@gmail.com>
2024-09-18 23:17:19 +0300

    update active appointement page

--- app/(Routes)/admin/dashboard/appointments/active/page.tsx
+++ app/(Routes)/admin/dashboard/appointments/active/page.tsx
@@ -2,81 +2,99 @@
 
 import DashboardTitle from "@/components/ui/dashboardTitle";
 import { DataTable } from "@/components/ui/DataTable";
+import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 import axios from "axios";
+import React, { useEffect } from "react";
+import { useAuth } from "@/context/authContext";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 // Define the Payment type
 type Payment = {
   id: number;
-  firstname: string;
+  patientName: string;
+  phoneNumber: string;
+  status: "PENDING" | "ACCEPTED" | "REJECTED";
   email: string;
-  phone: string;
-  role: string;
 };
 
 // Define the columns for the DataTable
 const columns: ColumnDef<Payment>[] = [
   {
-    accessorKey: "firstname",
+    accessorKey: "id",
+    header: "Order Id",
+    cell: ({ row }) => row.getValue("id"),
+  },
+  {
+    accessorKey: "patientName",
     header: "Name",
+    cell: ({ row }) => row.getValue("patientName"),
+  },
+  {
+    accessorKey: "phoneNumber",
+    header: "Phone",
+    cell: ({ row }) => row.getValue("phoneNumber"),
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
     cell: ({ row }) => (
-      <div className="flex items-center gap-2">
-        <Image
-          className="w-8 h-8 rounded-full"
-          src={`https://api.dicebear.com/9.x/lorelei/svg?seed=${row.getValue(
-            "firstname"
-          )}`}
-          alt="avatar"
-          width={32}
-          height={32}
-        />
-        <span>{row.getValue("firstname")}</span>
+      <div
+        className={cn("font-medium w-fit px-4 py-2 rounded-lg", {
+          "bg-red-200": row.getValue("status") === "PENDING",
+          "bg-orange-200": row.getValue("status") === "REJECTED",
+          "bg-green-200": row.getValue("status") === "ACCEPTED",
+        })}
+      >
+        {row.getValue("status")}
       </div>
     ),
   },
   {
     accessorKey: "email",
     header: "Email",
-  },
-  {
-    accessorKey: "phone",
-    header: "Phone",
-  },
-  {
-    accessorKey: "role",
-    header: "Role",
+    cell: ({ row }) => row.getValue("email"),
   },
 ];
 
-const DashboardUsers: React.FC = () => {
-  const [data, setData] = useState<Payment[]>([]);
+const DashboardOrders = () => {
+  const { userData } = useAuth();
   const router = useRouter();
+  const [data, setData] = React.useState<Payment[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/api/users`);
+        const res = await axios.get(`/api/appointement/all`);
+        console.log("res", res.data.data);
         setData(res.data.data);
       } catch (error) {
-        console.error("Error fetching users data:", error);
+        console.log("Error", error);
       }
     };
     fetchData();
   }, []);
 
   const handleRowClick = (rowData: Payment) => {
-    router.push(`/admin/dashboard/users/${rowData.id}`);
+    router.push(`/admin/dashboard/appointments/active/${rowData.id}`);
   };
 
   return (
     <div className="flex flex-col gap-5 w-full">
-      <DashboardTitle title="Users" />
+      <DashboardTitle title="Rejected Orders" />{" "}
+      {/* Optional: Add a title component */}
       <DataTable columns={columns} data={data} onRowClick={handleRowClick} />
     </div>
   );
 };
 
-export default DashboardUsers;
+export default DashboardOrders;
