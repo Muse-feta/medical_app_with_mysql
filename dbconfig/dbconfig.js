9351b3a91e260afe1140201a7d1773a6136325bf
Muse-feta <fetamuse@gmail.com>
2024-09-18 22:47:01 +0300

    change certs folder

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -1,17 +1,18 @@
-// const mysql = require('mysql2/promise');
-import mysql from "mysql2/promise"
-import fs from 'fs';
+import mysql from "mysql2/promise";
+import fs from "fs";
+import path from "path";
 
+// Construct the path to the certificate file in the `public` folder
+const certPath = path.join(process.cwd(), "public", "certs", "isrgrootx1.pem");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        ca: fs.readFileSync(process.env.CA),
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(certPath),
+  },
 });
 
-
-export default pool
+export default pool;
